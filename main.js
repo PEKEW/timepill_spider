13f53a7f46f270846fef8481549abe91c723cf47
PEKEW <pekewang@hotmail.com>
2022-02-11 13:40:34 +0800

    首次提交
    首次提交 杨思思的胶囊爬虫

---
+++ main.js
@@ -0,0 +1,40 @@
+  
+  // 引入模块
+  const { app, BrowserWindow } = require('electron')
+  
+
+  // 创建窗口
+  function createWindow () {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    })
+  
+    // 加载模版
+    win.loadFile('index.html')
+
+    // 开发者工具
+    win.webContents.openDevTools()
+
+  }
+  
+  // 初始化后调用
+  app.whenReady().then(() => {
+    createWindow()
+  })
+  
+  // 管理生命周期
+  // 如果不是mac(darwin) 则所有窗口被关闭时 应用退出
+  // 在mac上 需要cmd Q显示地退出程序
+  app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+
+    // 针对mac 如果没有任何窗口时被激活 则创建一个窗口
+    // 在whenready回调中注册监听器 监听activate事件 
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+      })
+  })
---
+++ preload.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
+
+// 在文件头部引入 Node.js 中的 path 模块
+const path = require('path')
+
+// 修改现有的 createWindow() 函数
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+// ...
